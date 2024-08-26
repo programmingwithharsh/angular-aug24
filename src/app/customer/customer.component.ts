afe7a3f61decf600102025c0ace8dc9d3db930d7
Harsh Tiwari <harshtiwari343@email.com>
2024-08-26 10:35:35 +0530

    Added Reactive Form

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../customer.service';
 import { ICustomer } from '../ICustomer';
-import { NgIf, NgFor } from '@angular/common';
+import { NgIf, NgFor, JsonPipe, NgClass } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, ReactiveFormsModule, FormsModule, JsonPipe, NgClass],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.scss'
 })
@@ -14,15 +15,34 @@ export class CustomerComponent implements OnInit {
 
   customers!: ICustomer[];
   errorMessage = "";
-  constructor(private customerServ: CustomerService) { }
+  customerForm!: FormGroup;
+  constructor(private customerServ: CustomerService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.getCustomersList();
+    this.createCustomerForm();
+  }
+
+  getCustomersList() {
     this.customerServ.getCustomers().subscribe({
       next: customers => {
         this.customers = customers
       },
       error: err => this.errorMessage = err
     })
+  }
+
+  createCustomerForm() {
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]]
+    })
+    console.log(this.customerForm.value);
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log(this.customerForm.value);
   }
 
 }
